33d6a3c49854dae51ca813f6f6c29be69d54ac10
Martin Fesenko <Mfesenko2003@gmail.com>
2022-11-20 18:01:29 +0200

    vectors

---
+++ C#/algebra-of-logic/algebra-of-logic/Program.cs
@@ -0,0 +1,70 @@
+﻿using static System.Console;
+using static System.Convert;
+
+int numDim, n;
+string vector_string;
+string duplicate;
+string substr;
+int[] vector;
+bool issuperech = true;
+
+do
+{
+    numDim = 0;
+    WriteLine("Enter vector of values of a logical function through spacebar: ");
+    vector_string = ReadLine();
+    duplicate = string.Copy(vector_string);
+    
+    if ((vector_string.Contains('0') || vector_string.Contains('1')) && vector_string.Contains(' ') && vector_string.Length > 2)
+        
+        while (vector_string.Length > 0)
+        {
+            n = vector_string.LastIndexOf(' ');
+            substr = vector_string.Substring(n + 1);
+            
+            if (substr != "1" && substr != "0") { numDim = 0; break; }
+            else
+            {
+                numDim++; if (n < 0) break; vector_string = vector_string.Remove(n);
+            }
+        }
+} while (numDim == 0 || checkLog2Digit(numDim) == -1);
+
+vector = new int[numDim];
+
+for (int i = 0; i < numDim; i++) vector[i] = duplicate[2 * i] - 48;
+Write("Vector: (");
+
+for (int i = 0; i < numDim; i++)
+{
+    if (i == numDim - 1) Write(vector[i]);
+    else Write($"{vector[i]}, ");
+}
+
+Write(")\n");
+
+if (vector[0] == 0) WriteLine("Функція зберігає константу 0.");
+else WriteLine("Функція не зберігає константу 0.");
+
+for (int i = 0, k = numDim - 1; i < numDim / 2; k--, i++)
+    
+    if (vector[i] != 0)
+    {
+        WriteLine("Функція не є суперечністю.");
+        issuperech = false;
+        break;
+    }
+
+if (issuperech)
+    WriteLine("Функція є суперечністю.");
+
+static int checkLog2Digit(int digit)
+{
+    if (digit % 2 == 1) return -1;
+    
+    digit /= 2;
+    
+    if (digit == 1) return 0;
+    
+    return checkLog2Digit(digit);
+}
